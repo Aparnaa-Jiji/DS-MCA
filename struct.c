7852e109e767f88ff6c575afd6ce7130371e95af
Aparnaa-Jiji <aparnajiji003@gmail.com>
2024-12-09 10:30:54 +0530

    Add files via upload

---
+++ struct.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct Student{
+	char name[50];
+	int id ;
+	int age;
+};
+
+int main()
+{
+	struct Student student ={"Steeve",135,21};
+	printf("Stdent Details:\n");
+	printf("Name:%s\n",student.name);
+	printf("ID:%d\n",student.id);
+	printf("Age:%d\n",student.age);
+	
+	return 0;
+}
+	
